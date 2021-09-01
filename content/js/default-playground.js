badcac3f8238a767da7a803df0325decba314fb4
Latonal <reflexion85@gmail.com>
2021-09-01 20:10:48 +0200

    fixes + adding groups

--- content/js/default-playground.js
+++ content/js/default-playground.js
@@ -3,7 +3,7 @@ var defaultPlayground = {
     "target": true,
     "playground": [
         {
-            "name": "1 ienb",
+            "name": "ntm hemz <3",
             "content": [
                 {
                     "categories": [
@@ -19,35 +19,35 @@ var defaultPlayground = {
                                 {
                                     "text": "Gmail",
                                     "url": "https://mail.google.com/mail/u/0/?hl=fr#inbox",
-                                    "icon": "./content/img/logo/gm.png",
+                                    "icon": "gm.png",
                                     "customcss": "background-color:#111;",
                                     "target": true
                                 },
                                 {
                                     "text": "Google Drive",
                                     "url": "https://drive.google.com/drive/u/0/my-drive",
-                                    "icon": "./content/img/logo/drive.png",
+                                    "icon": "drive.png",
                                     "customcss": "",
                                     "target": true
                                 },
                                 {
                                     "text": "Youtube",
                                     "url": "https://www.youtube.com/",
-                                    "icon": "./content/img/logo/yt.png",
+                                    "icon": "yt.png",
                                     "customcss": "",
                                     "target": true
                                 },
                                 {
                                     "text": "Soundcloud",
                                     "url": "https://soundcloud.com/",
-                                    "icon": "./content/img/logo/sc.png",
+                                    "icon": "sc.png",
                                     "customcss": "",
                                     "target": true
                                 },
                                 {
                                     "text": "GTrad",
                                     "url": "https://translate.google.fr/",
-                                    "icon": "./content/img/logo/gtranslate.png",
+                                    "icon": "gtranslate.png",
                                     "customcss": "",
                                     "target": true
                                 },
@@ -65,21 +65,21 @@ var defaultPlayground = {
                                 {
                                     "text": "Mémoires",
                                     "url": "https://drive.google.com/drive/u/7/folders/1V1eT3yejpcM42SfSviNnIjDNJ28dR4V4",
-                                    "icon": "./content/img/logo/drive.png",
+                                    "icon": "drive.png",
                                     "customcss": "",
                                     "target": true
                                 },
                                 {
                                     "text": "Drawing Outfit RPG",
                                     "url": "https://www.bing.com/images/search?view=detailV2&ccid=Y8GCqXQC&id=3B3F7464E67AA1FF495A12C84B2EF236BB8D7229&thid=OIP.Y8GCqXQC9QzPQSaSwNJd-gAAAA&mediaurl=https%3a%2f%2fi.pinimg.com%2foriginals%2fca%2f0e%2fba%2fca0eba8d9a1b13548ef2d4807e9f0b0c.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR63c182a97402f50ccf412692c0d25dfa%3frik%3dKXKNuzbyLkvIEg%26pid%3dImgRaw&exph=780&expw=460&q=drawing+outfit+rpg&simid=608054690747607916&ck=59E83FEDDE9F88CD9A7530C66F40E9D0&selectedIndex=38&FORM=IRPRST&ajaxhist=0&ajaxserp=0",
-                                    "icon": "./content/img/logo/bing.png",
+                                    "icon": "bing.png",
                                     "customcss": "",
                                     "target": true
                                 },
                                 {
                                     "text": "Unity Editor Custom Grid",
                                     "url": "https://www.bing.com/search?q=unity+custom+editor+grid&qs=n&form=QBRE&sp=-1&pq=unity+custom+editor+grid&sc=0-24&sk=&cvid=A2291DB6FB1A47FCB57F3B3199CE805C",
-                                    "icon": "./content/img/logo/bing.png",
+                                    "icon": "bing.png",
                                     "customcss": "",
                                     "target": true
                                 }
