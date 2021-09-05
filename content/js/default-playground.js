32139c4df7780f2d04a1ee609c5e5b7adec81a26
Latonal <reflexion85@gmail.com>
2021-09-05 21:13:36 +0200

    corrected issue with group not removed when they should

--- content/js/default-playground.js
+++ content/js/default-playground.js
@@ -9,6 +9,7 @@ var defaultPlayground = {
                     "categories": [
                         {
                             "name": "empty-box",
+                            "uuid": "2bf7a657-72c3-4832-b56c-fa8fd5de7a48",
                             "customcss": "min-height:55vh;"
                         }
                     ]
@@ -17,6 +18,7 @@ var defaultPlayground = {
                     "categories": [
                         {
                             "name": "icon-list",
+                            "uuid": "365eac08-cf01-463e-8d05-d07c41c92b45",
                             "customcss": "",
                             "target": true,
                             "links": [
@@ -59,6 +61,7 @@ var defaultPlayground = {
                         },
                         {
                             "name": "sub-list",
+                            "uuid": "1f83fd06-1118-4a4b-94c3-2cca92099ae5",
                             "customcss": "",
                             "target": true,
                             "links": [
@@ -91,6 +94,7 @@ var defaultPlayground = {
                     "categories": [
                         {
                             "name": "empty-box",
+                            "uuid": "68b7483f-767f-4d27-8588-4bf2f1060198",
                             "customcss": "min-height:40px;"
                         }
                     ]
@@ -99,6 +103,7 @@ var defaultPlayground = {
                     "categories": [
                         {
                             "name": "empty-box",
+                            "uuid": "d7686260-13b2-4f03-88e1-d5f2427dd59c",
                             "customcss": "min-height:40px;"
                         }
                     ]
