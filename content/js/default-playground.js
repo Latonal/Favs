1d13be6735253046b3b34582124bf6ada49f7d9d
Latonal <52961796+Latonal@users.noreply.github.com>
2021-09-28 15:23:11 +0200

    fixed JSON bug when moving category + continued creation of items

--- content/js/default-playground.js
+++ content/js/default-playground.js
@@ -11,7 +11,16 @@ var defaultPlayground = {
                         {
                             "name": "empty-box",
                             "uuid": "2bf7a657-72c3-4832-b56c-fa8fd5de7a48",
-                            "customcss": "min-height:55vh;"
+                            "customcss": "min-height:40px;"
+                        }
+                    ]
+                },
+                {
+                    "categories": [
+                        {
+                            "name": "empty-box",
+                            "uuid": "68b7483f-767f-4d27-8588-4bf2f1060198",
+                            "customcss": "min-height:80px;"
                         }
                     ]
                 },
@@ -27,6 +36,7 @@ var defaultPlayground = {
                                     "text": "Gmail",
                                     "url": "https://mail.google.com/mail/u/0/?hl=fr#inbox",
                                     "icon": "gm.png",
+                                    "uuid": "365eac08-cf01-463e-8d05-d07c41c92b451",
                                     "customcss": "background-color:#111;",
                                     "target": true
                                 },
@@ -34,6 +44,7 @@ var defaultPlayground = {
                                     "text": "Google Drive",
                                     "url": "https://drive.google.com/drive/u/0/my-drive",
                                     "icon": "drive.png",
+                                    "uuid": "365eac08-cf01-463e-8d05-d07c41c92b452",
                                     "customcss": "",
                                     "target": true
                                 },
@@ -41,6 +52,7 @@ var defaultPlayground = {
                                     "text": "Youtube",
                                     "url": "https://www.youtube.com/",
                                     "icon": "yt.png",
+                                    "uuid": "365eac08-cf01-463e-8d05-d07c41c92b453",
                                     "customcss": "",
                                     "target": true
                                 },
@@ -48,6 +60,7 @@ var defaultPlayground = {
                                     "text": "Soundcloud",
                                     "url": "https://soundcloud.com/",
                                     "icon": "sc.png",
+                                    "uuid": "365eac08-cf01-463e-8d05-d07c41c92b454",
                                     "customcss": "",
                                     "target": true
                                 },
@@ -55,6 +68,7 @@ var defaultPlayground = {
                                     "text": "GTrad",
                                     "url": "https://translate.google.fr/",
                                     "icon": "gtranslate.png",
+                                    "uuid": "365eac08-cf01-463e-8d05-d07c41c92b455",
                                     "customcss": "",
                                     "target": true
                                 },
@@ -70,6 +84,7 @@ var defaultPlayground = {
                                     "text": "Mémoires",
                                     "url": "https://drive.google.com/drive/u/7/folders/1V1eT3yejpcM42SfSviNnIjDNJ28dR4V4",
                                     "icon": "drive.png",
+                                    "uuid": "365eac08-cf01-463e-8d05-d07c41c92b456",
                                     "customcss": "",
                                     "target": true
                                 },
@@ -77,6 +92,7 @@ var defaultPlayground = {
                                     "text": "Drawing Outfit RPG",
                                     "url": "https://www.bing.com/images/search?view=detailV2&ccid=Y8GCqXQC&id=3B3F7464E67AA1FF495A12C84B2EF236BB8D7229&thid=OIP.Y8GCqXQC9QzPQSaSwNJd-gAAAA&mediaurl=https%3a%2f%2fi.pinimg.com%2foriginals%2fca%2f0e%2fba%2fca0eba8d9a1b13548ef2d4807e9f0b0c.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR63c182a97402f50ccf412692c0d25dfa%3frik%3dKXKNuzbyLkvIEg%26pid%3dImgRaw&exph=780&expw=460&q=drawing+outfit+rpg&simid=608054690747607916&ck=59E83FEDDE9F88CD9A7530C66F40E9D0&selectedIndex=38&FORM=IRPRST&ajaxhist=0&ajaxserp=0",
                                     "icon": "bing.png",
+                                    "uuid": "365eac08-cf01-463e-8d05-d07c41c92b457",
                                     "customcss": "",
                                     "target": true
                                 },
@@ -84,6 +100,7 @@ var defaultPlayground = {
                                     "text": "Unity Editor Custom Grid",
                                     "url": "https://www.bing.com/search?q=unity+custom+editor+grid&qs=n&form=QBRE&sp=-1&pq=unity+custom+editor+grid&sc=0-24&sk=&cvid=A2291DB6FB1A47FCB57F3B3199CE805C",
                                     "icon": "bing.png",
+                                    "uuid": "365eac08-cf01-463e-8d05-d07c41c92b458",
                                     "customcss": "",
                                     "target": true
                                 },
@@ -91,6 +108,7 @@ var defaultPlayground = {
                                     "text": "Useful links",
                                     "url": "https://docs.google.com/document/d/1Hy7agMqInEB_cjPixlLFgypadTfMm36frgjIhkKLQh4/edit",
                                     "icon": "bing.png",
+                                    "uuid": "365eac08-cf01-463e-8d05-d07c41c92b459",
                                     "customcss": "",
                                     "target": true
                                 }
@@ -102,17 +120,8 @@ var defaultPlayground = {
                     "categories": [
                         {
                             "name": "empty-box",
-                            "uuid": "68b7483f-767f-4d27-8588-4bf2f1060198",
-                            "customcss": "min-height:40px;"
-                        }
-                    ]
-                },
-                {
-                    "categories": [
-                        {
-                            "name": "empty-box",
                             "uuid": "d7686260-13b2-4f03-88e1-d5f2427dd59c",
-                            "customcss": "min-height:40px;"
+                            "customcss": "min-height:55vh;"
                         }
                     ]
                 }
