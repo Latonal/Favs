b105b177057dcaebe16960e69062c21682edbfe1
Latonal <reflexion85@gmail.com>
2021-11-02 18:26:10 +0100

    modified images, css to allow variables for themes and starting changing image

--- content/js/data.js
+++ content/js/data.js
@@ -1,7 +1,6 @@
 // using https://www.w3.org/TR/IndexedDB/ & https://www.w3docs.com/learn-javascript/javascript-indexeddb.html
-allowLocalStorage = false;
-DB_NAME = "playground-ltnl-461"; // please if you are copying my code, change the DB_NAME
-DB_VERSION = 1;
+const DB_NAME = "playground-ltnl-461"; // please if you are copying my code, change the DB_NAME
+const DB_VERSION = 1;
 data = null;
 
 // DEBUG
