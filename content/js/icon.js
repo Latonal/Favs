37c031b8417c6ea4576477c7aa17bbf9cd8bbb5a
Latonal <52961796+Latonal@users.noreply.github.com>
2021-11-02 18:26:10 +0100

    modified images, css to allow variables for themes and starting changing image

---
+++ content/js/icon.js
@@ -0,0 +1,8 @@
+var icon = {
+    "Gmail": "Gmail.svg",
+    "Google Drive": "GoogleDrive.svg",
+    "YouTube": "YouTube.svg",
+    "Soundcloud": "Soundcloud.svg",
+    "Google Translate": "GoogleTranslate.svg",
+    "Bing": "Bing.svg"
+}
