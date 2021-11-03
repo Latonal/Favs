a13a4a15798c9d221a59be5ccbfa6982fa712783
Latonal <52961796+Latonal@users.noreply.github.com>
2021-11-03 18:07:00 +0100

    continued themes

--- content/js/default-playground.js
+++ content/js/default-playground.js
@@ -5,13 +5,15 @@ var defaultPlayground = {
     "playground": [
         {
             "name": "ntm hemz <3",
-            "templatestyle": "",
+            "theme": "",
+            "customcss":"",
             "content": [
                 {
                     "categories": [
                         {
                             "name": "empty-box",
                             "uuid": "2bf7a657-72c3-4832-b56c-fa8fd5de7a48",
+                            "theme":"",
                             "customcss": "min-height:40px;"
                         }
                     ]
@@ -21,6 +23,7 @@ var defaultPlayground = {
                         {
                             "name": "empty-box",
                             "uuid": "68b7483f-767f-4d27-8588-4bf2f1060198",
+                            "theme":"",
                             "customcss": "min-height:80px;"
                         }
                     ]
@@ -30,6 +33,7 @@ var defaultPlayground = {
                         {
                             "name": "icon-list",
                             "uuid": "365eac08-cf01-463e-8d05-d07c41c92b45",
+                            "theme":"",
                             "customcss": "",
                             "target": true,
                             "links": [
@@ -46,6 +50,7 @@ var defaultPlayground = {
                                     "url": "https://drive.google.com/drive/u/0/my-drive",
                                     "icon": "GoogleDrive.svg",
                                     "uuid": "365eac08-cf01-463e-8d05-d07c41c92b452",
+                                    "theme":"",
                                     "customcss": "",
                                     "target": true
                                 },
@@ -54,6 +59,7 @@ var defaultPlayground = {
                                     "url": "https://www.youtube.com/",
                                     "icon": "YouTube.svg",
                                     "uuid": "365eac08-cf01-463e-8d05-d07c41c92b453",
+                                    "theme":"",
                                     "customcss": "",
                                     "target": true
                                 },
@@ -62,6 +68,7 @@ var defaultPlayground = {
                                     "url": "https://soundcloud.com/",
                                     "icon": "Soundcloud.svg",
                                     "uuid": "365eac08-cf01-463e-8d05-d07c41c92b454",
+                                    "theme":"",
                                     "customcss": "",
                                     "target": true
                                 },
@@ -70,6 +77,7 @@ var defaultPlayground = {
                                     "url": "https://translate.google.fr/",
                                     "icon": "GoogleTranslate.svg",
                                     "uuid": "365eac08-cf01-463e-8d05-d07c41c92b455",
+                                    "theme":"",
                                     "customcss": "",
                                     "target": true
                                 },
@@ -78,6 +86,7 @@ var defaultPlayground = {
                         {
                             "name": "sub-list",
                             "uuid": "1f83fd06-1118-4a4b-94c3-2cca92099ae5",
+                            "theme": "",
                             "customcss": "",
                             "target": true,
                             "links": [
@@ -86,6 +95,7 @@ var defaultPlayground = {
                                     "url": "https://drive.google.com/drive/u/7/folders/1V1eT3yejpcM42SfSviNnIjDNJ28dR4V4",
                                     "icon": "GoogleDrive.svg",
                                     "uuid": "365eac08-cf01-463e-8d05-d07c41c92b456",
+                                    "theme":"veranda",
                                     "customcss": "",
                                     "target": true
                                 },
@@ -94,6 +104,7 @@ var defaultPlayground = {
                                     "url": "https://www.bing.com/images/search?view=detailV2&ccid=Y8GCqXQC&id=3B3F7464E67AA1FF495A12C84B2EF236BB8D7229&thid=OIP.Y8GCqXQC9QzPQSaSwNJd-gAAAA&mediaurl=https%3a%2f%2fi.pinimg.com%2foriginals%2fca%2f0e%2fba%2fca0eba8d9a1b13548ef2d4807e9f0b0c.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR63c182a97402f50ccf412692c0d25dfa%3frik%3dKXKNuzbyLkvIEg%26pid%3dImgRaw&exph=780&expw=460&q=drawing+outfit+rpg&simid=608054690747607916&ck=59E83FEDDE9F88CD9A7530C66F40E9D0&selectedIndex=38&FORM=IRPRST&ajaxhist=0&ajaxserp=0",
                                     "icon": "Bing.svg",
                                     "uuid": "365eac08-cf01-463e-8d05-d07c41c92b457",
+                                    "theme":"",
                                     "customcss": "",
                                     "target": true
                                 },
@@ -102,6 +113,7 @@ var defaultPlayground = {
                                     "url": "https://www.bing.com/search?q=unity+custom+editor+grid&qs=n&form=QBRE&sp=-1&pq=unity+custom+editor+grid&sc=0-24&sk=&cvid=A2291DB6FB1A47FCB57F3B3199CE805C",
                                     "icon": "Bing.svg",
                                     "uuid": "365eac08-cf01-463e-8d05-d07c41c92b458",
+                                    "theme":"",
                                     "customcss": "",
                                     "target": true
                                 },
@@ -110,6 +122,7 @@ var defaultPlayground = {
                                     "url": "https://docs.google.com/document/d/1Hy7agMqInEB_cjPixlLFgypadTfMm36frgjIhkKLQh4/edit",
                                     "icon": "Bing.svg",
                                     "uuid": "365eac08-cf01-463e-8d05-d07c41c92b459",
+                                    "theme":"",
                                     "customcss": "",
                                     "target": true
                                 }
@@ -122,6 +135,7 @@ var defaultPlayground = {
                         {
                             "name": "empty-box",
                             "uuid": "d7686260-13b2-4f03-88e1-d5f2427dd59c",
+                            "theme":"",
                             "customcss": "min-height:55vh;"
                         }
                     ]
