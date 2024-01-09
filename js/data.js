6c1294b8d283e9cedff84636b8b26ddd9dede467
Latonal <reflexion85@gmail.com>
2024-01-10 00:42:04 +0100

    initial commit of v1

---
+++ js/data.js
@@ -0,0 +1,161 @@
+const DB_NAME = "playground-ltnl-462";
+const DB_VERSION = 1;
+
+// export {
+//     instantiateDB,
+//     openDatabase,
+//     deleteDB
+// }
+
+function instantiateDB() {
+    // TODO : replace this openRequest by openDatabase()
+    const openRequest = indexedDB.open(DB_NAME, DB_VERSION); //
+    let db; //
+
+    return new Promise((resolve, reject) => {
+        openRequest.onupgradeneeded = function (event) {
+            const db = openRequest.result;
+
+            if (event.oldVersion < 1) { // Initialize db
+                const elementsStore = db.createObjectStore("elements", { keyPath: "uuid" })
+                elementsStore.createIndex("by_parent", "parent", { unique: false });
+
+                const iconsStore = db.createObjectStore("icons", { keyPath: "uuid" });
+                iconsStore.createIndex("by_name", "name", { unique: false });
+                iconsStore.createIndex("link", "link", { unique: false });
+
+                const informationsStore = db.createObjectStore("informations", { autoIncrement: true });
+                informationsStore.createIndex("by_parent", "parent", { unique: false });
+
+
+                defaultGeneration(elementsStore, iconsStore, informationsStore);
+
+                StartTutorial();
+            }
+        };
+        openRequest.onerror = function () {
+            console.error("ERROR Database-2:\nAn error occured while opening the database: ", openRequest.error);
+            // reject("ERROR Database-2:\nAn error occured while opening the database: \n" + openRequest.error);
+            reject(false);
+        };
+        openRequest.onsuccess = function () {
+            db = openRequest.result;
+            console.log("Database has been opened: ", db);
+            // continue using the db object
+            let elements = db.transaction("elements", "readonly");
+            let elem = elements.objectStore("elements");
+
+            // get by index
+            let parent = elem.index("by_parent");
+            console.log("get elements with parent 1:", parent.getAll(1));
+
+            // empty index is not possible, we use a dummy data
+            console.log("by empty index: ", parent.getAll(IDBKeyRange.only(0)));
+
+            // get by key
+            let withKey = elem.get(1);
+            console.log("with key:", withKey);
+
+            // get only data related to key
+            withKey.onsuccess = function () {
+                const matching = withKey.result;
+                console.log("parent:", matching.parent, " id:", matching.uuid);
+            }
+
+            // TODO : CURSOR
+
+            db.close();
+            resolve(true);
+        };
+    });
+}
+
+function openDatabase() {
+    return new Promise((resolve, reject) => {
+        const openRequest = indexedDB.open(DB_NAME, DB_VERSION);
+        // const openRequest = IndexedDBWrapper.open(DB_NAME, DB_VERSION);
+        // const openRequest = FavsIndexedDB.open(DB_NAME, DB_VERSION);
+
+        openRequest.onerror = function () {
+            console.error("ERROR Database-1:\nAn error occured while opening the database: ", openRequest.error);
+            reject(false);
+        };
+
+        openRequest.onsuccess = function () {
+            resolve(openRequest.result);
+        };
+    });
+}
+
+function deleteDB() {
+    let deleteRequest = indexedDB.deleteDatabase(DB_NAME);
+    deleteRequest.onerror = function () {
+        console.error("ERROR Database-3:\nAn error occured while deleting the database", deleteRequest.error);
+    }
+    deleteRequest.onsuccess = function () {
+        console.log("Database has successfully been deleted");
+    }
+    console.log("database has been deleted");
+}
+
+function defaultGeneration(elementsStore, iconsStore, informationsStore) {
+    // ELEMENTS
+    // Page 1
+    elementsStore.put({ parent: 0, uuid: 1 });
+        elementsStore.put({ parent: 1, uuid: 2 });
+            elementsStore.put({ parent: 2, uuid: 3 });
+                elementsStore.put({ parent: 3, uuid: 4 });
+                elementsStore.put({ parent: 3, uuid: 19 });
+                elementsStore.put({ parent: 3, uuid: 20 });
+                elementsStore.put({ parent: 3, uuid: 21 });
+            elementsStore.put({ parent: 2, uuid: 5 });
+                elementsStore.put({ parent: 5, uuid: 6 });
+            elementsStore.put({ parent: 2, uuid: 7 });
+                elementsStore.put({ parent: 7, uuid: 8 });
+        elementsStore.put({ parent: 1, uuid: 9 });
+            elementsStore.put({ parent: 9, uuid: 10 });
+        elementsStore.put({ parent: 1, uuid: 11 });
+            elementsStore.put({ parent: 11, uuid: 12 });
+
+    // Page 2
+    elementsStore.put({ parent: 0, uuid: 13 });
+        elementsStore.put({ parent: 13, uuid: 14 });
+            elementsStore.put({ parent: 14, uuid: 15 });
+    // Page 3
+    elementsStore.put({ parent: 0, uuid: 16 });
+        elementsStore.put({ parent: 16, uuid: 17 });
+            elementsStore.put({ parent: 17, uuid: 18 });
+
+    // ICONS
+    iconsStore.put({ name: "options", link: "./img/options.svg", uuid: 1 });
+    iconsStore.put({ name: "blah blah", link: "./img/blahblah", uuid: 2 });
+
+    // INFORMATIONS
+    informationsStore.put({ parent: 1, order: 0, theme: "light" });
+    informationsStore.put({ parent: 2, order: 0 });
+    informationsStore.put({ parent: 3, order: 1 });
+    informationsStore.put({ parent: 4, order: 0, text: "middle element", img_uuid: 1 });
+    informationsStore.put({ parent: 19, order: 1, text: "element 1", img_uuid: 1 });
+    informationsStore.put({ parent: 20, order: 2, text: "number 2", img_uuid: 1 });
+    informationsStore.put({ parent: 21, order: 3, text: "element numéro 3", img_uuid: 1 });
+    informationsStore.put({ parent: 5, order: 0 });
+    informationsStore.put({ parent: 6, order: 0, text: "left element" });
+    informationsStore.put({ parent: 7, order: 2 });
+    informationsStore.put({ parent: 8, order: 2, text: "right element" });
+    informationsStore.put({ parent: 9, order: 1 });
+    informationsStore.put({ parent: 10, order: 0, text: "sticker below" });
+    informationsStore.put({ parent: 11, order: 2 });
+    informationsStore.put({ parent: 12, order: 0, text: "another sticker below" });
+    informationsStore.put({ parent: 19, order: 1 });
+
+    informationsStore.put({ parent: 13, order: 1, customcss: "background-color:green;" });
+    informationsStore.put({ parent: 14, order: 0 });
+    informationsStore.put({ parent: 15, order: 0, text: "some text on the second page" });
+    informationsStore.put({ parent: 16, order: 2, customcss: "background-color:blue;" });
+    informationsStore.put({ parent: 17, order: 0 });
+    informationsStore.put({ parent: 18, order: 0, text: "some text on the third page" });
+
+    // informationsStore.put({ parent: 12, img_uuid: 1, text: "some text", order: 2, color: "#3e3e3e" });
+    // informationsStore.put({ parent: 13, img_uuid: 2, order: 1, color: "blue", customcss: "font-size:16px;font-weight:bold;" });
+    // informationsStore.put({ parent: 11 });
+}
---
+++ js/tools.js
@@ -0,0 +1,112 @@
+// TODO : ENCRYPT
+String.prototype.encrypt/*, Number.prototype.encrypt */ = function () {
+    if (isEmpty(this)) return this;
+    return this;
+}
+// TODO : DECRYPT
+String.prototype.decrypt = function () {
+    if (isEmpty(this)) return this;
+    return this;
+}
+console.log(String.prototype);
+
+const FavsCustomElementsName = {
+    tags: {
+        ALBUM: "my-album",
+        GROUP: "my-group",
+        STICKER: "my-sticker",
+    }
+};
+
+class Sticker extends HTMLElement {
+    connectedCallback() {
+        console.log(this);
+        // this.firstChild.remove();
+    }
+}
+customElements.define(FavsCustomElementsName.tags.STICKER, Sticker);
+
+/** Generate a uuidv4 */
+function uuidv4() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+
+/** Create a new uuid but verify beforehand if it doesn't already exists
+ * @param {string} text text preceding the type of id we want to check, like "cat-", "it-"
+ * @returns {string} return a uuid in string format */
+function getRandomNonUsedUUID(text) {
+    while (true) {
+        var uuid = uuidv4();
+        if (!document.getElementById(text + uuid))
+            return uuid;
+    }
+}
+
+function isEmpty(e) {
+    if ((typeof e === "string" || typeof e === "object")
+        && e.length === 0) return true;
+    return false;
+}
+
+function isDefined(e) {
+    if (typeof (e) !== "undefined") return true;
+    return false;
+}
+
+function cleanArrayOfObjects(arr) {
+    return arr.filter(obj => {
+        return Object.values(obj).some(value => value !== null && value !== undefined && value != '');
+    });
+}
+
+const cssVariablePrefix = "--";
+/**
+ * Dissociate variables values from constants values in css
+ * @param {HTMLElement} str
+ */
+function dissociateCss(str) {
+    if (isEmpty(str) || !isDefined(str)) return;
+    const rules = str.split(';');
+
+    const values = {};
+
+    rules.forEach(rule => {
+        if (isEmpty(rule)) return;
+
+        let [property, value] = rule.split(":");
+        property = property.trim();
+        value = value.trim();
+
+        if (property.startsWith('--')) {
+            property = property.replace("--", "");
+            values[property] = value;
+        } else {
+            val = property + ":" + value + ";";
+            if (isDefined(values["customcss"])) values["customcss"] += val;
+            else values["customcss"] = val;
+        }
+    });
+
+    return values;
+}
+
+/**
+ * Associate css rules to fit in the style attribute
+ * @param  {...any} args 
+ * @returns 
+ */
+function associateCss(...args) {
+    args = cleanArrayOfObjects(args);
+    if (isEmpty(args)) return;
+    var css = "";
+    args.forEach(element => {
+        if (typeof (element) === "object") {
+            let property = Object.getOwnPropertyNames(element);
+            if (property == "customcss") css += element[property];
+            else css += "--" + property + ":" + element[property] + ";";
+        }
+    });
+    return css;
+}
