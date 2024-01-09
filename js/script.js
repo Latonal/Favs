98279524fe4d7773a79c4284c256495714257ab6
Latonal <52961796+Latonal@users.noreply.github.com>
2024-01-10 00:42:04 +0100

    initial commit of v1

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// import { instantiateDB, openDatabase, deleteDB } from "./data";
+
+Start();
+
+async function Start() {
+    if (!window.indexedDB) {
+        console.error("ERROR Browser-1:\nYour browser does not support IndexedDB. Update your browser or download one supporting IndexedDB. Browser supporting IndexedDB are listed here: https://caniuse.com/indexeddb");
+        return;
+    }
+
+    try {
+        const isok = await instantiateDB();
+        if (isok) {
+            generatePlayground().then(() => {
+                console.log("LAST: playground has been created");
+            }).catch((error) => {
+                console.error("playground has not been created: ", error);
+            })
+        }
+    } catch (error) {
+        console.error("ERROR Script-1:\nAn undetermined error occured: ", error);
+    }
+}
+
+function StartTutorial() {
+    console.log("TODO: starting tutorial");
+}
