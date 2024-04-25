08dc41c9eaf2d43a4bb9c41338594ac8296b2f93
Latonal <reflexion85@gmail.com>
2024-04-25 13:55:00 +0200

    added test execution time

--- js/test/test.js
+++ js/test/test.js
@@ -1,5 +1,8 @@
 function test() {
+    start = performance.now();
     testSort();
+    end = performance.now();
+    console.log("Tests executed in", end - start ,"ms");
 }
 
 function appendStringConsole(originalString, stringToAdd) {
